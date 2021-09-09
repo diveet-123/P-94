dfe007714cf8b34cbd286b7a36273deb93cbc7e9
diveet-123 <84632888+diveet-123@users.noreply.github.com>
2021-09-09 08:26:17 +0530

    Add files via upload

---
+++ Kwitter.js
@@ -0,0 +1,7 @@
+
+   function login_user()
+   {
+    username = document.getElementById("user_name").value;
+    localStorage.setItem("name", username);
+    window.location="Kwitter_room.html";
+   }
---
+++ Kwitter_room.js
@@ -0,0 +1,21 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCTvfJOkS3-8hLdI3X7RrUMihFeVu0Tke0",
+    authDomain: "project-93-94-95-96-867a2.firebaseapp.com",
+    databaseURL: "https://project-93-94-95-96-867a2-default-rtdb.firebaseio.com",
+    projectId: "project-93-94-95-96-867a2",
+    storageBucket: "project-93-94-95-96-867a2.appspot.com",
+    messagingSenderId: "261213594823",
+    appId: "1:261213594823:web:2d8e85a2bd212ab4520ae2"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  function addroom()
+  {
+    user_name_typed = document.getElementById("room_name").value;
+    firebase.database().ref("/").child(user_name_typed).update({
+        purpose:"adding user"
+    });
+  }
